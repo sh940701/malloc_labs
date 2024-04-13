737dd9a1e7140ed091d8b28f308e771c5e8b1c9c
sh940701 <visioner2168@gmail.com>
2024-04-13 06:45:24 +0000

    feat: malloc skeleton init

--- mm.c
+++ mm.c
@@ -18,6 +18,12 @@
 #include "mm.h"
 #include "memlib.h"
 
+static char *heap_listp;
+
+#define WSIZE 4 // word size
+#define DSIZE 8 // double word size
+#define CHUNKSIZE (1 << 12) // 페이지 증가 요청 크기. 연산 결과는 4kb 에 해당한다.
+
 /* single word (4) or double word (8) alignment */
 // word == 4 byte, double word align 정책을 적용함
 #define ALIGNMENT 8
@@ -47,27 +53,74 @@
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
 // 블록 포인터인 bp 를 받아 -4 byte 를 하여 header 의 주소를 read (이 때 bp 는 블록 내 payload 의 위치를 가리키는 것으로 보임)
-#define HDRP(bp) ((char *)(bp) - sizeof(size_t))
+#define HDRP(bp) ((char *)(bp) - WSIZE)
 
 // 블록 포인터인 bp 를 받아 bp 에서 block size 만큼을 더한 후, block size 단위(여기선 8 byte) 만큼 빼줌
 // 이 때 bp 가 block 의 header 부터가 아닌 payload 부터라고 하면, footer 의 크기인 4 byte 가 아니라 footer + header 의 크기인 8 byte 만큼 빼 주는 것이 이해 됨
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - ALIGNMENT)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 // 여기서 bp 또한 payload 의 시작 주소를 나타낸다고 볼 수 있다.
 // 1. payload addr - 4 byte 를 하여 header 의 주소를 구함
 // 2. header 에서 block 의 크기를 구함
 // 3. payload addr 에서 block 의 크기만큼 더하여, 다음 블록의 payload 의 주소를 반환
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - sizeof(size_t))))
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 
 // 1. 현재 payload 주소에서 8 byte(현재 블록의 header, 이전 블록의 footer) 만큼 빼서 이전 블록의 footer 위치 파악
 // 2. 이전 블록의 footer 에서 이전 블록의 크기를 구한 후, 현재 payload 의 위치에서 빼면, 이전 블록의 payload 주소값을 구할 수 있음
-#defind PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - ALIGNMENT)))
+#defind PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *) (bp) - DSIZE)))
 
-/*
- * mm_init - initialize the malloc package.
- */
-int mm_init(void) {
-    return 0;
+static void *coalesce(void *bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    // Case 1
+    // 전, 후 block 이 모두 allocation 되어있으면 연결 불가
+    if (prev_alloc && next_alloc) {
+        return bp;
+    }
+
+        // Case 2
+    else if (prev_alloc && !nest_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+        // Case 3
+    else if (!prev_allc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+        // Case 4
+    else if (!prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    return bp;
+}
+
+static void *extend_heap(size_t words) {
+    char *bp;
+    size_t size;
+
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if ((long) (bp = mem_sbrk(size) == (void *) -1)) {
+        return NULL;
+    }
+
+    PUT(HDRP(bp), PACK(size, 0)); // free block header
+    PUT(FTRP(bp), PACK(size, 0)); // free block footer
+    PUT(HDRP(NEXT_BLKP(bp), PACK(0, 1))) // new epilogue header -> 에필로그 헤더를 뒤로 밈.
+
+    // Coalesce 구현
+    return coalesce(bp);
 }
 
 /*
@@ -75,20 +128,45 @@ int mm_init(void) {
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size) {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *) -1)
+    size_t asize; // adjusted block size
+    size_t extendsize; // amount to extend heap if no fit
+    char *bp;
+
+    if (size == 0) {
         return NULL;
-    else {
-        *(size_t *) p = size;
-        return (void *) ((char *) p + SIZE_T_SIZE);
     }
+
+    // header(4) + footer(4) + payload => multiples of DSIZE
+    if (size <= DSIZE) {
+        asize = 2 * DSIZE
+    } else {
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) /
+                         DSIZE); // 7을 더한 후 DSIZE 로 나눠서 몫만 챙김 -> payload 가 들어갈 수 있는 DSIZE 의 배수 중 최소값을 구함
+    }
+
+    if ((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
+    }
+
+    extendsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendsize / WSIZE)) == NULL) {
+        return NULL;
+    }
+
+    place(bp, asize);
+    return bp;
 }
 
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr) {
+void mm_free(void *bp) {
+    size_t size = GET_SIZE(HDRP(bp));
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    coalesce(bp);
 }
 
 /*
@@ -110,6 +188,27 @@ void *mm_realloc(void *ptr, size_t size) {
     return newptr;
 }
 
+/*
+ * mm_init - initialize the malloc package.
+ */
+int mm_init(void) {
+    // sbrk 를 호출하여 메모리 할당
+    // 이 때 초기값으로 Alignment padding, Prologue header/footer, Epilogue header 를 만들어주기 때문에 4 word 를 할당해준다.
+    if ((heap_listp = mem_sbrk(4 * sizeof(size_t)) == (void *) -1)) {
+        return -1;
+    }
+
+    PUT(heap_listp, 0); // Alignment padding
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1)); // Prologue header
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1)); // Prologue footer
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1)); // Epilogue header
+    heap_listp += (2 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE / WSIZE) == NULL) { // 할당이 안 되면(?)
+        return -1
+    }
+    return 0;
+}
 
 
 
