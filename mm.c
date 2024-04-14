70f3b2d1c3627ba5f7757a7d7f1f3ba46e45ead2
sh940701 <visioner2168@gmail.com>
2024-04-14 09:31:44 +0000

    fix: invalid values

--- mm.c
+++ mm.c
@@ -145,7 +145,7 @@ static void *find_fit(size_t asize) {
     void *bp;
 
     for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-        if (!GET_SIZE(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
             return bp;
         }
     }
@@ -248,7 +248,7 @@ void *mm_realloc(void *ptr, size_t size) {
     newptr = mm_malloc(size);
     if (newptr == NULL)
         return NULL;
-    copySize = *(size_t * )((char *) oldptr - SIZE_T_SIZE);
+    copySize = GET_SIZE(HDRP(oldptr));
     if (size < copySize)
         copySize = size;
     memcpy(newptr, oldptr, copySize);
